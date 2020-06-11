90aee65f89ecd5deb67c5f305cbad84dc8c5d4df
BickhamM21 <BickhamM21@ehsbr.org>
2020-06-10 22:24:55 -0500

    Chunk management system file created
    Chunk management system file created

---
+++ Assets/Scripts/ECS/Systems/ChunkManagementSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+
+using Unity.Mathematics;
+
+
+using UnityEngine;
+
+public class ChunkManagementSystem : SystemBase
+{
+
+    const int viewDistance = 100; //max view distance in terms of chunks, each chunk is 16mx16m. this results in a view distance of 1.6km in each direction
+
+    NativeHashMap<float3,Entity> chunks;
+
+    protected override void OnCreate()
+    {
+
+        chunks = new NativeHashMap<float3, Entity>(viewDistance * viewDistance, Allocator.Persistent);
+
+    }
+    protected override void OnUpdate()
+    {
+
+
+
+    }
+
+
+    protected override void OnDestroy()
+    {
+        chunks.Dispose();
+
+
+    }
+    
+}
