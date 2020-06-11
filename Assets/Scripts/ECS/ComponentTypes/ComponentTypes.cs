b988deb6b40fd9c9398c85106061b8d17e185f01
BickhamM21 <BickhamM21@ehsbr.org>
2020-06-10 22:11:39 -0500

    Basic file system along with basic voxel data structure component
    Basic file system along with basic voxel data structure component

---
+++ Assets/Scripts/ECS/ComponentTypes/ComponentTypes.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+
+[InternalBufferCapacity(4096)]
+struct VoxelData : IBufferElementData {
+    float value;
+    float3 normal;
+    int material;
+}
